dd9fb2bad0c7cece6f5f75f7583b838f78b0fa62
Ronald Post <ronald.post@magnaversum.com>
2018-11-14 09:45:01 +0100

    initial commit

---
+++ MixedAuthTest/Pages/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MixedAuthTest.Pages.Controllers
+{    
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : Controller
+    {        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return NoContent();
+        }
+    }
+}
--- MixedAuthTest/Startup.cs
+++ MixedAuthTest/Startup.cs
@@ -1,17 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MixedAuthTest.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace MixedAuthTest
 {
@@ -40,8 +40,36 @@ namespace MixedAuthTest
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            services.AddAuthentication()
+                .AddJwtBearer(jwtBearerOptions =>
+                {
+                    jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = Configuration["Issuer"],
+                        ValidAudience = Configuration["Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SigningKey"])),
+                    };
+                });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //Always return 401
+            //services.AddMvc(config =>
+            //{
+            //    var policy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+            //                     .RequireAuthenticatedUser()
+            //                     .Build();
+            //    config.Filters.Add(new AuthorizeFilter(policy));
+            //}).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Always return loging page
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder(IdentityConstants.ApplicationScheme,
+                                                            JwtBearerDefaults.AuthenticationScheme)
+                                 .RequireAuthenticatedUser()
+                                 .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,8 +91,25 @@ namespace MixedAuthTest
             app.UseCookiePolicy();
 
             app.UseAuthentication();
+            
+            // Always returns 401 
+            //app.UseMvc(config =>
+            //{
+            //    var defaultPolicy = new AuthorizationPolicyBuilder(new[] 
+            //            { JwtBearerDefaults.AuthenticationScheme })
+            //        .RequireAuthenticatedUser()
+            //        .Build();
+            //});
 
-            app.UseMvc();
+            // Always returns login page
+            app.UseMvc(config =>
+            {
+                var defaultPolicy = new AuthorizationPolicyBuilder(new[]
+                        { IdentityConstants.ApplicationScheme,
+                          JwtBearerDefaults.AuthenticationScheme })
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
         }
     }
 }
